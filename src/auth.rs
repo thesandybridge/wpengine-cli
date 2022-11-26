57b90c9b4df8357d204dc5cb83e4052d064308c7
Matt Miller <mattmillerf@pm.me>
2022-11-25 23:47:42 -0500

    add authentication check

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,7 @@
 use home_config::HomeConfig;
 use std::io;
 use std::str;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 
@@ -18,33 +19,48 @@ fn set_config(username: String, token: String) {
         wpengine_user_id: username,
         wpengine_password: token
     };
-    println!("{:?}", data);
     config.save_toml(&data).unwrap();
+}
+
+fn authenticated() -> bool {
+    let config = HomeConfig::with_config_dir("wpe", "wpeconfig.toml");
+    let data: Data = config.toml().unwrap();
+    let re = Regex::new(r"/([0-9A-Z]{8})(-)([0-9A-F]{4})(-)([0-9A-Z]{4})(-)([0-9A-Z]{4})(-)([0-9A-Z]{12})/i").unwrap();
+    
+    if re.is_match(&data.wpengine_password) {
+        true
+    } else {
+        false
+    }
 }
 
 /// Handles user authentication.
 pub fn authenticate() {
-    println!("Authenticate with wpengine.");
 
-    let mut username = String::new();
-    let mut token = String::new();
+    if !authenticated() {
+        println!("Authenticate with wpengine.");
 
-    println!("Enter API Username:");
-
-    io::stdin()
-        .read_line(&mut username)
-        .expect("Failed to read line");
+        let mut username = String::new();
+        let mut token = String::new();
     
-    let trimmed_user = username.trim();
-
-    println!("Enter API Password:");
-
-    io::stdin()
-        .read_line(&mut token)
-        .expect("Failed to read line");
+        println!("Enter API Username:");
     
-    let trimmed_token = token.trim();
+        io::stdin()
+            .read_line(&mut username)
+            .expect("Failed to read line");
+        
+        let trimmed_user = username.trim();
     
-    set_config(trimmed_user.to_string(), trimmed_token.to_string());
+        println!("Enter API Password:");
+    
+        io::stdin()
+            .read_line(&mut token)
+            .expect("Failed to read line");
+        
+        let trimmed_token = token.trim();
+        
+        set_config(trimmed_user.to_string(), trimmed_token.to_string());
+    }
+
 
 }
