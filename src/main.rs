4a84a7cb60b2b86c1e144b4641feafd2331c1c2e
Matt Miller <mattmillerf@pm.me>
2023-01-17 15:27:37 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,27 @@ impl Site {
     }
 
     /// Get all sites from wpengine API
-    pub fn get_sites(&self, config: &wpe::Data) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn get_sites(&self, config: &wpe::Data) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
         let res = self.client.get(&format!("{}/sites", &config.wpengine_api))
             .basic_auth(&config.wpengine_user_id, Some(&config.wpengine_password))
             .send()?
             .json::<serde_json::Value>()?;
 
-        for i in res["results"].as_array().unwrap() {
-            println!("{} = {}", i["name"], i["id"]);
-        }
-        Ok(())
+        Ok(res)
     }
 
     /// Get a single site by its ID from the wpengine API
-    pub fn get_site_by_id(&self, config: &wpe::Data, id: &str) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn get_site_by_id(&self, config: &wpe::Data, id: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
         let res = self.client.get(&format!("{}/sites/{}", &config.wpengine_api,  id))
             .basic_auth(&config.wpengine_user_id, Some(&config.wpengine_password))
             .send()?
             .json::<serde_json::Value>()?;
 
         println!("{}", serde_json::to_string_pretty(&res)?);
-        Ok(())
+        Ok(res)
     }
 }
 
-/// Handles the CLI using clap.
 fn cli() -> Command {
     Command::new("wpe")
         .about("WPEngine CLI")
@@ -80,11 +76,15 @@ fn main() -> Result<()> {
     // Switch to listen for commands and execute proper functions.
     match matches.subcommand() {
         Some(("sites", _)) => {
-            site.get_sites(&config).unwrap();
+            let res = site.get_sites(&config).unwrap();
+            for i in res["results"].as_array().unwrap() {
+                println!("{} = {}", i["name"], i["id"]);
+            }
         },
         Some(("site", sub_m)) => {
             let id = sub_m.get_one::<String>("ID").unwrap();
-            site.get_site_by_id(&config, id).unwrap();
+            let res = site.get_site_by_id(&config, id).unwrap();
+            println!("{}", serde_json::to_string_pretty(&res)?);
         },
         Some(("auth", sub_m)) => {
             match sub_m.subcommand() {
